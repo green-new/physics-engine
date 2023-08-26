c68133b8158257368b1c0f4fce8503a2d4645dba
green-new <tristenwoodruff2@gmail.com>
2023-08-26 13:36:37 -0400

    Insertion and creating useful functions

--- physics-engine/octree.hpp
+++ physics-engine/octree.hpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 
 namespace Physics {
-	/* Octree containing entities that could be colliding */
+	/* octree containing entities that could be colliding */
 	class CollisionTree {
 	private:
 		BoundingBox m_region;
@@ -73,52 +73,71 @@ namespace Physics {
 			m_pending.push(object);
 		}
 
+		// Move collider from this to child
 		void moveInto(Physics::Collider& collider, unsigned char index) {
 			m_used |= (unsigned char)(1 << index);
 			m_children[index]->m_colliders.push_back(std::move(collider));
 		}
 
+		/* ONLY WORKS moving colliders down 1 level */
 		void moveCollidersDown() {
 			// Check which regions our colliders are in
 			for (auto it = m_colliders.begin(); it != m_colliders.end(); ++it) {
 				for (unsigned char i = 0; i < 8; i++) {
 					CollisionTree* child = m_children[i];
-					/* Move our collider down the node */
+
+					// Move our collider down the node
 					if ((*it).getRegion().overlaps(child->getRegion())) {
 						moveInto(*it, i);
+
+						// if we have too many entities, split
+						if (child->m_colliders.size() > maxEntities) {
+							child->build();
+						}
 					}
 				}
 			}
 		}
 
-		/* Get the largest power of 2 cube enclosing all current entities */
+		/* get the largest power of 2 cube enclosing entities down this node (primarily used with the root node) */
 		void solveInitialSpace() {
-			signed int min = -1, max = 1;
-			BoundingBox enclosingCube(min, min, min, max, max, max);
-			// TODO: List vs vector here
-			std::list<Physics::Collider&> dummy(m_colliders.begin(), m_colliders.end());
-			while (!dummy.empty()) {
-				enclosingCube.min = glm::vec3(min, min, min); enclosingCube.max = glm::vec3(max, max, max);
-				dummy.remove_if([enclosingCube](Physics::Collider c) {
-					c.getRegion().overlaps(enclosingCube);
-					});
-				min << 1;			// Check if endianness matters on other machines
-				max << 1;
+
+			// Must be root node or we get weird octants
+			if (m_parent != nullptr) {
+
+				// See if there are any colliders not within the current region
+				bool orphansExist = false;
+				for (auto& collider : m_colliders) {
+					if (!collider.getRegion().overlaps(m_region)) {
+						orphansExist = true;
+						break;
+					}
+				}
+
+				if (orphansExist) {
+					signed int min = -1, max = 1;
+					// 0, 0, 0 should be the center (world coordinates)
+					BoundingBox enclosingCube(min, min, min, max, max, max);
+					// TODO: List vs vector here
+					std::list<Physics::Collider&> dummy(m_colliders.begin(), m_colliders.end());
+					while (!dummy.empty()) {
+						enclosingCube.min = glm::vec3(min, min, min); enclosingCube.max = glm::vec3(max, max, max);
+						dummy.remove_if([enclosingCube](Physics::Collider c) {
+							c.getRegion().overlaps(enclosingCube);
+							});
+						min << 1;			// Check if endianness matters on other machines
+						max << 1;
+					}
+					m_region = enclosingCube;
+				}
 			}
-			m_region = enclosingCube;
 		}
 
-		/* Recursive function that builds the tree */
-		void build() {
-			// Base case
-			if (m_colliders.size() <= maxEntities) {
-				return;
-			}
-
+		void createChildren() {
 			// True center of the bounding box
 			glm::vec3 center = m_region.center();
 
-			// Utilize the heap and not the stack
+			// Utilize the heap and not the stack because our octree is probably gonna be huge
 			for (unsigned char i = 0; i < 8; i++) {
 				m_children[i] = new CollisionTree(BoundingBox(), std::vector<Physics::Collider>(), this);
 
@@ -132,25 +151,54 @@ namespace Physics {
 					(i & 4) ? center.z : m_region.min.z
 				);
 				m_children[i]->m_region.max = glm::vec3(
-					(i & 1) ? center.x : m_region.max.x,
-					(i & 2) ? center.y : m_region.max.y,
-					(i & 4) ? center.z : m_region.max.z
+					(i & 1) ? m_region.max.x : center.x,
+					(i & 2) ? m_region.max.y : center.y,
+					(i & 4) ? m_region.max.z : center.z
 				);
 			}
+		}
+
+		/* Recursive function that builds the tree from the current position */
+		void build() {
+			// Base case
+			if (m_colliders.size() <= maxEntities) {
+				return;
+			}
+
+			createChildren();
 
 			moveCollidersDown();
 		}
 
-		// Start at the root node and insert the collider
-		// Assumes the current object (this) is the root node
+		/* Start at the root node and insert the collider */
+		/* ONLY WORKS moving colliders down 1 level */
 		void insertAtRoot(Physics::Collider* collider) {
+			if (m_parent != nullptr) {
+				if (m_isLeaf) {
+					createChildren();
+				}
+				for (unsigned char i = 0; i < 8; i++) {
+					auto child = m_children[i];
+					if (child->m_region.overlaps(collider->getRegion())) {
+						// Only insert if we are a leaf (smallest subregion that contains this region)
+						if (m_isLeaf) {
+							m_children[i]->m_colliders.push_back(*collider);
+						}
+						insertAtRoot(collider);
+					}
+				}
+			}
+		}
+
+		/* start at any node and find where to insert the collider */
+		/* ONLY WORKS moving colliders down 1 level */
+		void insertAtNode(Physics::Collider* collider) {
+			if (m_isLeaf) {
+				createChildren();
+			}
 			for (unsigned char i = 0; i < 8; i++) {
 				auto child = m_children[i];
-				if (!child->m_region.overlaps(collider->getRegion())) {
-					// Base case
-					return;
-				}
-				else {
+				if (child->m_region.overlaps(collider->getRegion())) {
 					// Only insert if we are a leaf
 					if (m_isLeaf) {
 						m_children[i]->m_colliders.push_back(*collider);
@@ -158,11 +206,9 @@ namespace Physics {
 					insertAtRoot(collider);
 				}
 			}
-		}
 
-		// Start at any node and find where to insert the collider
-		void insertAtNode(Physics::Collider* collider) {
-
+			// Base case
+			return;
 		}
 	};
 }
